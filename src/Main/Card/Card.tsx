a96d8009d6b57ccc4dfd8188885e54dbe790d487
Poldyaich97 <daniil.poldyaev97@gmail.com>
2022-11-25 13:06:18 +0500

    feat: working with styles

--- src/Main/Card/Card.tsx
+++ src/Main/Card/Card.tsx
@@ -6,10 +6,13 @@ export default function Card() {
 
     return (
         <div className={stylesCard.wrapper}>
-            <div className={stylesCard.info}>
-                <h4 className={stylesCard.name}>5-й эллемент</h4>
-                <p className={stylesCard.description}>Екатеринубрг,Радищева 28, 621</p>
-            </div>
+            <div className={stylesCard.infoKiosk}>
+                <div className={stylesCard.stat}></div>
+                <div className={stylesCard.info}>
+                    <h3 className={stylesCard.name}>5-й эллемент</h3>
+                    <p className={stylesCard.description}>Екатеринубрг,Радищева 28, 621</p>
+                </div></div>
+            <p className={stylesCard.status}>В сети</p>
             <p className={stylesCard.version}>1.240</p>
             <p className={stylesCard.namePC}>355g23</p>
 
