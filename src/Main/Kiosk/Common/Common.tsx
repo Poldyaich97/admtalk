c9f102ff31d13748eaa4c227f2c5b8f63a18ab27
Poldyaich97 <daniil.poldyaev97@gmail.com>
2023-02-11 15:19:29 +0500

    feat: add  Card & Common

---
+++ src/Main/Kiosk/Common/Common.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import stylesCard from ".//Common.module.css"
+
+const linkStyle = {
+    textDecoration: "none"
+}
+
+const Common: React.FC<any> = (props) => {
+    console.log(props)
+    return (
+        <div className={stylesCard.wrapper}>
+            <h4 className={stylesCard.information}>Параметры</h4>
+        </div>
+    )
+}
+export default Common;
+
