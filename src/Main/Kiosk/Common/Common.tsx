499f796b3e1cd37178c78cbeeeb3fa9e740aa48e
Poldyaich97 <daniil.poldyaev97@gmail.com>
2023-03-11 14:57:38 +0500

    add: added information to kiosk settings

--- src/Main/Kiosk/Common/Common.tsx
+++ src/Main/Kiosk/Common/Common.tsx
@@ -7,10 +7,15 @@ const Common: React.FC<any> = (props) => {
   return (
     <div className={styles.wrapper}>
       <h4 className={styles.information}>Параметры</h4>
-      <Information parmOne={'Название киоска:'} parmTwo={props.data.title} />
+      <Information parmOne={'Название киоска:'} parmTwo={props.data.title || 'Имя не задано'} />
       <Information parmOne={'Имя ПК:'} parmTwo={props.data.machineName} />
       <Information parmOne={'Версия:'} parmTwo={props.data.version} />
       <Information parmOne={'Ключ киоска:'} parmTwo={props.data.kioskKey} />
+      <h4 className={styles.information}>Каледарь</h4>
+      <p className={styles.callenfrInfo}>
+        Функция календаря позволяет отображать расписание переговорной комнаты. Укажите
+        эл.&nbsp;почту переговорной комнаты на сервисе календарей.
+      </p>
     </div>
   )
 }
